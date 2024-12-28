f51cff91759698447375a9ca2507ea19dc20bbc3
Experimenters1 <huydeveloper.52@gmail.com>
2024-12-28 16:36:48 +0700

    Initial Commit

---
+++ GITHUBDemo/GITHUBDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GITHUBDemoApp.swift
+//  GITHUBDemo
+//
+//  Created by Huy vu on 28/12/24.
+//
+
+import SwiftUI
+
+@main
+struct GITHUBDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
